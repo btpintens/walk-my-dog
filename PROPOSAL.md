b83bd86f9272f32340c1e48b70a98d63840271bd
Bri Pintens <btpintens@gmail.com>
2025-03-10 13:24:11 -0400

    updated markdown and pseudocode

--- PROPOSAL.md
+++ PROPOSAL.md
@@ -4,6 +4,9 @@
 )
 * As a user I want an introduction page that explains the requirements (rules) of walking my dog
 * As a user I want a clean interface with graphics of the dog and the "tools" you need to walk him (leash, bags, treats)
+* As a user I want a constant visual reminder of how many tools (kind and quantity) I have left
 * As a user I want buttons which clearly provide information regarding the choices I can make to progress the walk: ___give treats, look out for squirrels, pick up poop, etc.___
 * As a user I want to be able to click the buttons - thus moving onto the next part of the walk
 * As a user I want to be presented with a message when I have successfully completed the walk and an image the dog has made it safely home
+* As a user I want to know how much money I paid (points I have) once I have completed the walk (finished the game)
+
---
+++ app.js
@@ -0,0 +1,38 @@
+//HOME PAGE
+// DISPLAY ELEMENT: introductory message with image of dog and 
+// instructions of how to walk (what you'll encounter along the way)
+//BUTTON start walk button addEventListener
+
+//ICON/IMAGE table that lists tools (leash, bags, treats) we have 
+//and how much money earned. 
+
+//PAGE 2
+//DISPLAY ELEMENT dog pooped, what do you do
+//BUTTON A: leave it (return home. you failed) FAIL PAGE aEL
+//BUTTON B: pick it up (good job! keep walking) (decrease bags by 1
+// (but add $5?)) PROCEED TO PAGE 3 aEL
+//BUTTON C: ask a stranger to pick it up (return home. you failed) FAIL PAGE aEL
+
+//PAGE 3
+// DISPLAY: dog sees squirrel, what do you do 
+//BUTTON D: let him chase it 
+//  (return home, you lost my dog) FAIL PAGE aEL
+//BUTTON E: you both chase it together 
+//  (return home you both died) FAIL PAGE aEL
+//BUTTON F: hold that leash tight! PROCEED TO PAGE 4 aEL
+//  (good job! keep walking) (add another $5)
+
+//PAGE 4
+//DISPLAY ELEMENT there's a stranger approaching, what do you do
+//BUTTON G: nothing 
+//  (he bites stranger. you're fired. go home) FAIL PAGE aEL
+//BUTTON H: ask dog nicely to not bite the stranger 
+//  (this dog does not respect you. go home) FAIL PAGE aEL
+//BUTTON I: offer dog a treat as a distraction and a reward for good behavior 
+//  (good job! keep walking) (lose a treat but get another $5) THANK YOU FOR WALKING MY DOG aEL
+
+//VAR leash, bags, treats, money 
+//VAR buttons (3 per page, 9 total):  
+//IF proceed = increase money by 5. buttonB = reduce bags by 1. 
+//buttonI = reduce treats by 1.
+
